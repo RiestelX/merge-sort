5dfbda944f0d34443c8a160098c991678a5222e9
RiestelX <legonot@hotmail.com>
2024-06-03 23:24:11 +0700

    delete 3rd test since it dup with first test

--- src/__tests__/merge.test.ts
+++ src/__tests__/merge.test.ts
@@ -18,13 +18,4 @@ describe('merge function', () => {
         const result = merge(collection_1, collection_2, collection_3);
         expect(result).toEqual([1, 2, 3, 4, 5, 6]);
     });
-
-    it('dynamic length', () => {
-        const collection_1 = [8, 6, 4];
-        const collection_2 = [1, 2];
-        const collection_3 = [3, 5, 7, 9];
-
-        const result = merge(collection_1, collection_2, collection_3);
-        expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    });
 });
